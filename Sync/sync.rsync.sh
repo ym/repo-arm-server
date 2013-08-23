ac9a070a74440f386b16d4d5e18672855ad5ef9a
Felix Yan <felixonmars@gmail.com>
2013-08-23 23:47:42 +0800

    initial version of sync.rsync.sh

--- Sync/sync.rsync.sh
+++ Sync/sync.rsync.sh
@@ -1,2 +1,30 @@
-#!/bin/bash 
-# Bash sync script using rsync
+#!/bin/bash
+REPOS=(testing core extra community-testing community multilib-testing multilib)
+ARCHS=(i686 x86_64)
+DESTARCHS=(i686 x86_64 any)
+UPSTREAM=rsync://mirror.us.leaseweb.net/archlinux
+PKGDEST=/var/www/repo-arm
+DBBACKUP=/var/www/repo-arm-db
+
+for _r in ${REPOS[@]}; do
+  for _a in ${DESTARCHS[@]}; do
+    mkdir -p "$PKGDEST"/$_r/os/$_a
+  done
+done
+
+DATEROOT="$DBBACKUP"/$(date +%Y)/$(date +%M)/$(date +%d)
+rsync -avhkPSH "$UPSTREAM"
+
+for _r in ${REPOS[@]}; do
+  rsync -avhkPSH "$UPSTREAM"/$_r "$PKGDEST"/
+
+  for _a in ${ARCHS[@]}; do
+    if [[ -n "$DBBACKUP" ]]; then
+      mkdir -p "$DATEROOT"/$_r/os/$_a
+      cp "$PKGDEST"/$_r/os/$_a/${_r}.{db*,files*} "$DATEROOT"/$_r/os/$_a/
+    fi
+  done
+done
+
+rsync -avhkPSH "$UPSTREAM"/pool "$PKGDEST"/
+
