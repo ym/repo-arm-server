cd6293bcd7dc1d62d7c2acd65cec7eb26a755672
Phoenix Nemo <mrphoenixlzx@gmail.com>
2013-08-24 01:34:19 +0800

    fixed typo

--- repo-arm-server/config.js
+++ repo-arm-server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     pkginfopath: "/var/www/repo-arm", // pkginfo.db file path, without the last slash "/".
-    downloadurl: "http://repo-arm-download.archlinuxcn.org"  // package download url, for returning package download links.
+    downloadurl: "http://repo-arm-download.archlinuxcn.org/"  // package download url, for returning package download links.
 }
