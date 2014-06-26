080a18649b7cd1400df6e73d4030dd8b1a0d1b09
Phoenix Nemo <mrphoenixlzx@gmail.com>
2014-06-27 00:38:26 +0800

    updated readme & config

--- repo-arm-server/config.js
+++ repo-arm-server/config.js
@@ -1,4 +1,15 @@
 module.exports = {
     pkginfopath: "/var/www/repo-arm", // pkginfo.db file path, without the last slash "/".
-    downloadurl: "http://repo-arm-download.archlinuxcn.org/"  // package download url, for returning package download links.
+    downloadurl: "http://repo-arm-download.archlinuxcn.org/",  // package download url, for returning package download links.
+    
+    // archive db path
+    archivepath: '/var/www/repo-arm-db',
+    // packages path
+    pkgpath: '/var/www/repo-arm/',
+
+    // server config
+    // bind ip address
+    bindAddress: '127.0.0.1',
+    // bind port
+    bindPort: '3000'
 }
